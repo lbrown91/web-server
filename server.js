4f24216b7f22bbbb848054a7052759c016a4aab3
Lyell Brown <lyellbrown91@gmail.com>
2016-04-11 17:26:28 -0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();		// create express app by calling express function
+var PORT = 3000;		// uppercase var name says that var name will not change
+var date = new Date().toString();
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log('Request (' + date +  '): ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+// app.use(middleware.requireAuthentication);		// important to specify middleware sooner
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {  	// create about page at localhost:3000/about
+	res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
+});
